7fa38fe9db282dd44890404115f9cd8ed31fd120
Perry <olsson.perry@gmail.com>
2021-02-09 07:58:35 -0800

    migration

--- Api/src/prisma/migrations/20210209155821_init/migration.sql
+++ Api/src/prisma/migrations/20210209155821_init/migration.sql
@@ -1,22 +1,12 @@
-/*
-  Warnings:
-
-  - You are about to drop the `appointments` table. If the table is not empty, all the data it contains will be lost.
-
-*/
 -- CreateTable
 CREATE TABLE "Appointment" (
     "id" SERIAL NOT NULL,
-    "createAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "month" SMALLINT NOT NULL,
     "day" SMALLINT NOT NULL,
     "year" SMALLINT NOT NULL,
-    "time" SMALLINT NOT NULL,
     "timestamp" TIMESTAMP(3) NOT NULL,
 
     PRIMARY KEY ("id")
 );
-
--- DropTable
-DROP TABLE "appointments";
