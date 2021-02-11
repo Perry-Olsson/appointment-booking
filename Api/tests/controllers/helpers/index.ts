6cb07dabefd62b5017bda1f0e80682449716437a
Perry <olsson.perry@gmail.com>
2021-02-10 18:01:46 -0800

    fixed appointment sorting test bug

--- Api/tests/controllers/helpers/index.ts
+++ Api/tests/controllers/helpers/index.ts
@@ -16,12 +16,12 @@ export const parseRawAppointment = (appointment: any): Appointment => {
   return returnApp as Appointment;
 };
 
-export const appointmentsAreSorted = (appointments: any) => {
+export const appointmentsAreSorted = (appointments: Appointment[]) => {
   let lastAppointment = -Infinity;
   for (let appointment of appointments) {
-    if (appointment.timestamp < lastAppointment) return false;
+    if (appointment.timestamp.getTime() < lastAppointment) return false;
 
-    lastAppointment = appointment.timestamp;
+    lastAppointment = appointment.timestamp.getTime();
   }
   return true;
 };
