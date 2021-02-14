34b4e193cfda75bf44954de8b78941dff3811380
Perry Olsson <olsson.perry@gmail.com>
2021-02-14 12:01:38 -0800

    Logging the query object in logging middleware

--- Api/src/utils/middleware.ts
+++ Api/src/utils/middleware.ts
@@ -4,6 +4,7 @@ import logger from "./logger";
 export const requestLogger = (req: Request, _: any, next: NextFunction) => {
   logger.info("Method: ", req.method);
   logger.info("Path: ", req.path);
+  logger.info("Query: ", req.query);
   logger.info("Body: ", req.body);
   logger.info("--------------------");
   next();
