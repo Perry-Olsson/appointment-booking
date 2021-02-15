5c3b553113e3bc1f0467b643d114f6199dd6c234
Perry Olsson <olsson.perry@gmail.com>
2021-02-15 12:33:37 -0800

    refactored exhaustive if statement for cleanliness

--- Api/tests/controllers/appointments/helpers/parseRawAppointment.ts
+++ Api/tests/controllers/appointments/helpers/parseRawAppointment.ts
@@ -10,19 +10,24 @@ export const parseRawAppointment = (appointment: any): Appointment => {
     ) {
       returnApp[field] = new Date(appointment[field]);
     } else {
-      if (
-        field === "id" ||
-        field === "year" ||
-        field === "month" ||
-        field === "day" ||
-        field === "hour" ||
-        field === "minute"
-      )
-        returnApp[field] = appointment[field];
-      else throw new Error("Invalid appointment fields");
+      if (isValidAppointmentField(field)) returnApp[field] = appointment[field];
+      else throw new Error(`Invalid appointment field: ${field}`);
     }
   }
   if (Object.keys(returnApp).length !== 9)
     throw new Error("Missing fields in appointment");
   return returnApp as Appointment;
+};
+
+const isValidAppointmentField = (field: string): boolean => {
+  if (
+    field === "id" ||
+    field === "year" ||
+    field === "month" ||
+    field === "day" ||
+    field === "hour" ||
+    field === "minute"
+  )
+    return true;
+  else return false;
 };
