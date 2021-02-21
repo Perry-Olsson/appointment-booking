c7142e9e8648035c16daeeee2ebad2394de3486b
Perry <olsson.perry@gmail.com>
2021-02-20 16:38:24 -0800

    refactored validateQuery function

--- Api/src/repositories/Appointments/utils/validateQuery.ts
+++ Api/src/repositories/Appointments/utils/validateQuery.ts
@@ -1,51 +1,54 @@
 import { Prisma } from "@prisma/client";
 
 export const validateQuery = (query: any) => {
+  if (!query || !query.where) return {};
+  const whereInput = query.where;
   const validQuery: Prisma.AppointmentWhereInput = {};
 
-  if (!query || !query.where) return validQuery;
-  const whereInput = query.where;
-
-  const assignQueryField = (field: AppointmentField): void => {
-    const num = isNumber(whereInput[field]);
-    if (num !== false) validQuery[field] = num;
+  const assignQueryField = (field: string): void => {
+    if (isValidAppointmentField(field)) {
+      const num = toNumber(whereInput[field]);
+      if (num !== false) validQuery[field as AppointmentField] = num;
+    }
   };
 
-  for (let field in whereInput) {
-    switch (field) {
-      case "year":
-        assignQueryField("year");
-        break;
-      case "month":
-        assignQueryField("month");
-        break;
-      case "day":
-        assignQueryField("day");
-        break;
-      case "hour":
-        assignQueryField("hour");
-        break;
-      case "minute":
-        assignQueryField("minute");
-        break;
-    }
-  }
+  for (let field in whereInput) assignQueryField(field);
 
+  assignYearValue(validQuery);
+
+  return validQuery;
+};
+
+const isValidAppointmentField = (field: string): boolean => {
+  if (
+    field === "year" ||
+    field === "month" ||
+    field === "day" ||
+    field === "hour" ||
+    field === "minutes"
+  )
+    return true;
+  return false;
+};
+
+const toNumber = (queryArg: AppointmentField): number | false => {
+  const num = Number(queryArg);
+  if (isNaN(num)) {
+    return false;
+  } else return num;
+};
+
+/*
+Assign a year value that returns future appointments 
+if a month value and no year value is provided
+*/
+const assignYearValue = (validQuery: Prisma.AppointmentWhereInput): void => {
   if (typeof validQuery.month === "number" && !validQuery.year) {
     const now = new Date();
     if (now.getMonth() > validQuery.month)
       validQuery.year = now.getFullYear() + 1;
     else validQuery.year = now.getFullYear();
   }
-
-  return validQuery;
-};
-
-const isNumber = (queryArg: AppointmentField): number | false => {
-  const num = Number(queryArg);
-  if (isNaN(num)) {
-    return false;
-  } else return num;
 };
 
 type AppointmentField = "year" | "month" | "day" | "hour" | "minute";
