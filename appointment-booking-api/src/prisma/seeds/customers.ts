8fd8dfea5cd65b68cea2e6a7b99bbe1f9ff216ed
Perry <olsson.perry@gmail.com>
2021-03-18 16:18:21 -0700

    customer seed script working

--- appointment-booking-api/src/prisma/seeds/customers.ts
+++ appointment-booking-api/src/prisma/seeds/customers.ts
@@ -1,5 +1,21 @@
+import bcrypt from "bcryptjs";
 import { prisma } from "../prisma";
+import customers from "./json/customers.json";
+import { NewCustomer } from "../../types";
 
 export const seedCustomers = async () => {
   await prisma.customer.deleteMany();
+
+  for (let i = 0; i < customers.length; i++) {
+    const newCustomer = customers[i] as NewCustomer;
+
+    if (newCustomer.type === "USER")
+      newCustomer.password = await bcrypt.hash(newCustomer.password!, 8);
+
+    await prisma.customer.create({ data: newCustomer });
+  }
+
+  await prisma.$disconnect();
 };
+
+seedCustomers();
