bc5a426d253028250c1662905239cbb594e9ce61
Perry <olsson.perry@gmail.com>
2021-03-23 21:23:20 -0700

    tested and functional initialize function for user creation

--- appointment-booking-api/src/repositories/Customer/Customer.ts
+++ appointment-booking-api/src/repositories/Customer/Customer.ts
@@ -1,14 +1,20 @@
 import { EmailError, RequestBodyError } from "../../../src/utils";
 import validator from "email-validator";
+import bcrypt from "bcryptjs";
 import { Prisma } from ".prisma/client";
 
 //write password hash logic for USER type customers
 class _Customer {
-  public initialize(reqBody: any): Prisma.CustomerCreateInput {
+  public async initialize(reqBody: any): Promise<Prisma.CustomerCreateInput> {
     if (typeof reqBody !== "object") throw new RequestBodyError(reqBody);
 
     if (!this._validateEmail(reqBody.email))
       throw new EmailError(reqBody.email);
+
+    if (reqBody.type === "GUEST") delete reqBody.password;
+    else {
+      reqBody.password = await bcrypt.hash(reqBody.password, 8);
+    }
 
     return reqBody as Prisma.CustomerCreateInput;
   }
--- appointment-booking-api/tests/repositories/Customer.test.ts
+++ appointment-booking-api/tests/repositories/Customer.test.ts
@@ -1,6 +1,7 @@
 import { prisma } from "../../src/prisma";
 import { initializeTestData } from "../helpers";
 import { Customer } from "../../src/repositories/Customer";
+import bycrypt from "bcryptjs";
 
 beforeAll(async () => {
   await initializeTestData();
@@ -9,20 +10,39 @@ beforeAll(async () => {
 afterAll(() => prisma.$disconnect());
 
 describe("Customer Creation", () => {
-  test("Initialze function throws error if object isn't provided", () => {
-    expect(() => Customer.initialize("hello")).toThrow();
+  test("Initialze function throws error if object isn't provided", async () => {
+    await expect(Customer.initialize("hello")).rejects.toThrow();
   });
 
-  test("Initialize function validates email address correctly", () => {
-    const validCustomer = Customer.initialize(testUser);
-    expect(() =>
+  test("Initialize function validates email address correctly", async () => {
+    const validCustomer = await Customer.initialize(testUser);
+    await expect(
       Customer.initialize({
         ...testUser,
         email: "invalidEmail",
       })
-    ).toThrow();
+    ).rejects.toThrow();
 
     expect(validCustomer).toBeDefined();
+  });
+
+  test("Initialize function hashes user password and deletes guest password field", async () => {
+    const user = await Customer.initialize({ ...testUser });
+    const guest = await Customer.initialize({ ...testGuest });
+
+    expect(guest.password).toBeUndefined();
+    expect(user.password).toHaveLength(60);
+    expect(
+      await bycrypt.compare(testUser.password, user.password!)
+    ).toBeTruthy();
+  });
+
+  test("Initialize returns a valid customer", async () => {
+    const user = await Customer.initialize({ ...testUser });
+    const guest = await Customer.initialize({ ...testGuest });
+
+    expect(Object.keys(user).length).toBe(Object.keys(testUser).length);
+    expect(Object.keys(guest).length).toBe(Object.keys(testGuest).length - 1);
   });
 });
 
@@ -33,4 +53,13 @@ const testUser = {
   password: "steve12",
   firstName: "Steve",
   lastName: "Smith",
+};
+
+const testGuest = {
+  email: "bob@example.com",
+  type: "GUEST",
+  phoneNumber: "(123)-543-3212",
+  password: null,
+  firstName: "Bob",
+  lastName: "Dull",
 };
