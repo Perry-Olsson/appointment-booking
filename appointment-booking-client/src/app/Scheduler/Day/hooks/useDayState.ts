888332cb6583dce3324bc446bd4a6d76854226b4
Perry Olsson <olsson.perry@gmail.com>
2021-04-03 21:15:19 -0700

    setting appointments to prefetched appointments if available

--- appointment-booking-client/src/app/Scheduler/Day/hooks/useDayState.ts
+++ appointment-booking-client/src/app/Scheduler/Day/hooks/useDayState.ts
@@ -1,5 +1,5 @@
 import { useAtom } from "jotai";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useGetSelectedDay } from ".";
 import { monthsAtom } from "../../atoms";
 import { useFetchAppointments } from "../../hooks";
@@ -9,6 +9,10 @@ export const useDayState = () => {
   const [appointments, setAppointments] = useState(prefetchedAppointments);
   const { error } = useFetchAppointments(day, setAppointments);
   const [months, setMonths] = useAtom(monthsAtom);
+
+  useEffect(() => {
+    if (prefetchedAppointments) setAppointments(prefetchedAppointments);
+  }, [day.valueOf()]);
 
   return { day, appointments, months, setMonths, error };
 };
