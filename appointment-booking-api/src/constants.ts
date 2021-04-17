bd10894d52bf28c60ac4bf9e4d4c13f6b6c1d44d
Perry Olsson <olsson.perry@gmail.com>
2021-04-17 09:54:13 -0700

    testing schema options

--- appointment-booking-api/src/constants.ts
+++ appointment-booking-api/src/constants.ts
@@ -1,4 +1,6 @@
-export const QUARTER_HOUR = 1000 * 60 * 15;
+export const SECOND = 1000;
+export const MINUTE = SECOND * 60;
+export const QUARTER_HOUR = MINUTE * 15;
 export const HALF_HOUR = QUARTER_HOUR * 2;
 export const HOUR = HALF_HOUR * 2;
 export const ONE_DAY = 1000 * 60 * 60 * 24;
