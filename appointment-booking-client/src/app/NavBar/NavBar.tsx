cdd6950c5327e7b50c3a83c75fceee1d0dd17580
Perry Olsson <olsson.perry@gmail.com>
2021-04-25 11:41:11 -0700

    desktop navbar with login tab

--- appointment-booking-client/src/app/NavBar/NavBar.tsx
+++ appointment-booking-client/src/app/NavBar/NavBar.tsx
@@ -6,19 +6,25 @@ import { Tab } from "./Tab";
 export const NavBar: React.FC = () => {
   return (
     <Container>
-      <Tab href="/">Home</Tab>
-      <Tab href="/about">About</Tab>
-      <Tab href="/schedule">Book Online</Tab>
+      <Flex>
+        <Tab href="/">Home</Tab>
+        <Tab href="/about">About</Tab>
+        <Tab href="/schedule">Book Online</Tab>
+      </Flex>
+      <Flex>
+        <Tab href="/login">Log in</Tab>
+      </Flex>
     </Container>
   );
 };
 
-const Container = styled(Flex)`
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
   position: fixed;
   top: 0;
   width: 100%;
   z-index: 1;
-  justify-content: flex-start;
   height: ${({ theme }) => theme.navBar.height};
   color: ${({ theme }) => theme.colors.textSecondary};
   background-color: ${({ theme }) => theme.colors.primary};
