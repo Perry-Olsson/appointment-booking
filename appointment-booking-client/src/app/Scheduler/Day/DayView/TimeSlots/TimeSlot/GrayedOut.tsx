3cfb44e4e7809acea583f2bfea70b504acaea26f
Perry <olsson.perry@gmail.com>
2021-05-04 17:06:31 -0700

    added cursor pointer to available timeslots

--- appointment-booking-client/src/app/Scheduler/Day/DayView/TimeSlots/TimeSlot/GrayedOut.tsx
+++ appointment-booking-client/src/app/Scheduler/Day/DayView/TimeSlots/TimeSlot/GrayedOut.tsx
@@ -1,3 +1,4 @@
+import { FC } from "react";
 import styled from "styled-components";
 import { QUARTER_HOUR } from "../../../../../../constants";
 
@@ -7,26 +8,48 @@ interface GrayedOutProps {
   endValue: number;
 }
 
-export const GrayedOut = styled.div<GrayedOutProps>`
+export const GrayedOut: FC<GrayedOutProps> = props => {
+  return (
+    <Container
+      slotIsTaken={slotIsTaken(props)}
+      isStart={isStartOfAppointment(props)}
+      isEnd={isEndOfAppointment(props)}
+    >
+      {props.children}
+    </Container>
+  );
+};
+
+export const Container = styled.div<ContainerProps>`
   height: 100%;
-  background-color: ${({ theme, timeSlotValue, timestampValue, endValue }) =>
-    timeSlotValue >= timestampValue && timeSlotValue < endValue
-      ? theme.dayView.appointmentBlockColor
-      : null};
-  border-top-left-radius: ${({ theme, timeSlotValue, timestampValue }) =>
-    timeSlotValue === timestampValue
-      ? theme.dayView.appointmentBlockRadius
-      : null};
-  border-top-right-radius: ${({ theme, timeSlotValue, timestampValue }) =>
-    timeSlotValue === timestampValue
-      ? theme.dayView.appointmentBlockRadius
-      : null};
-  border-bottom-left-radius: ${({ theme, timeSlotValue, endValue }) =>
-    endValue && timeSlotValue === endValue - QUARTER_HOUR
-      ? theme.dayView.appointmentBlockRadius
-      : null};
-  border-bottom-right-radius: ${({ theme, timeSlotValue, endValue }) =>
-    endValue && timeSlotValue === endValue - QUARTER_HOUR
-      ? theme.dayView.appointmentBlockRadius
-      : null};
+  background-color: ${({ slotIsTaken }) => (slotIsTaken ? "gray" : null)};
+  border-top-left-radius: ${({ theme, isStart }) =>
+    isStart ? theme.dayView.appointmentBlockRadius : null};
+  border-top-right-radius: ${({ theme, isStart }) =>
+    isStart ? theme.dayView.appointmentBlockRadius : null};
+  border-bottom-left-radius: ${({ theme, isEnd }) =>
+    isEnd ? theme.dayView.appointmentBlockRadius : null};
+  border-bottom-right-radius: ${({ theme, isEnd }) =>
+    isEnd ? theme.dayView.appointmentBlockRadius : null};
 `;
+
+interface ContainerProps {
+  slotIsTaken: boolean;
+  isStart: boolean;
+  isEnd: boolean;
+}
+
+const slotIsTaken = ({
+  timeSlotValue,
+  timestampValue,
+  endValue,
+}: GrayedOutProps) =>
+  timeSlotValue >= timestampValue && timeSlotValue < endValue ? true : false;
+
+const isStartOfAppointment = ({
+  timeSlotValue,
+  timestampValue,
+}: GrayedOutProps) => (timeSlotValue === timestampValue ? true : false);
+
+const isEndOfAppointment = ({ timeSlotValue, endValue }: GrayedOutProps) =>
+  endValue && timeSlotValue === endValue - QUARTER_HOUR ? true : false;
--- appointment-booking-client/src/app/Scheduler/Day/DayView/TimeSlots/TimeSlot/utils/appointmentFitsSlot.ts
+++ appointment-booking-client/src/app/Scheduler/Day/DayView/TimeSlots/TimeSlot/utils/appointmentFitsSlot.ts
@@ -1,4 +1,44 @@
+import { getSelectedAppointment } from ".";
+import { Appointment, Procedure, Provider } from "../../../../../../../types";
 import { to4DigitTimeNumber } from "../../../../../utils";
+
+interface SelectedAppointmentFitsArgs {
+  provider: Provider | undefined;
+  procedure: Procedure | undefined;
+  timeSlot: Date;
+  time: Date | undefined;
+  appointment: Appointment | undefined;
+}
+
+export const selectedAppointmentFits = ({
+  provider,
+  procedure,
+  timeSlot,
+  time,
+  appointment,
+}: SelectedAppointmentFitsArgs) => {
+  if (provider && procedure) {
+    const selectedAppointment = getSelectedAppointment(time, procedure);
+    if (selectedAppointment?.start.valueOf() === timeSlot.valueOf()) {
+      return undefined;
+    }
+
+    const appointmentEnd = new Date(timeSlot);
+    appointmentEnd.setMinutes(timeSlot.getMinutes() + procedure.duration);
+
+    if (appointment) {
+      if (appointmentEnd > appointment.timestamp && timeSlot < appointment.end)
+        return false;
+    }
+
+    const schedule = provider.schedule[timeSlot.getDayString()];
+
+    if (appointmentFitsSlot(schedule, timeSlot, appointmentEnd)) {
+      return true;
+    }
+  }
+  return false;
+};
 
 export const appointmentFitsSlot = (
   schedule: string[],
