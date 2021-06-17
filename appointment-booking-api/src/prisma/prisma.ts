279c23562b5a0a11f25dfac9072d1a166141b80e
Perry Olsson <olsson.perry@gmail.com>
2021-06-16 22:15:09 -0700

    changed for db seeding purposes

--- appointment-booking-api/src/prisma/prisma.ts
+++ appointment-booking-api/src/prisma/prisma.ts
@@ -1,8 +1,9 @@
 import { Prisma, PrismaClient } from "@prisma/client";
-import config from "../config";
 
 const log: (Prisma.LogLevel | Prisma.LogDefinition)[] | undefined =
-  config.env === "development" ? ["query", "info", "warn", "error"] : undefined;
+  process.env.NODE_ENV === "development"
+    ? ["query", "info", "warn", "error"]
+    : undefined;
 
 export const prisma = new PrismaClient({
   log,
