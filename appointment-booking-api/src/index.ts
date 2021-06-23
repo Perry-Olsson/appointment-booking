5d28c61f6841a8791a47ea46eb57ece8ab3f919f
Perry Olsson <olsson.perry@gmail.com>
2021-06-22 19:02:50 -0700

    cron job for transfering past appointments setup

--- appointment-booking-api/src/index.ts
+++ appointment-booking-api/src/index.ts
@@ -1,6 +1,9 @@
 import http from "http";
 import { app } from "./app";
 import config from "./config";
+import { cronJobs } from "./utils";
+
+cronJobs();
 
 http.createServer(app).listen(config.port, () => {
   console.log(`server is listening on port ${config.port}`);
--- appointment-booking-api/src/utils/cron/index.ts
+++ appointment-booking-api/src/utils/cron/index.ts
@@ -1,1 +1,18 @@
+import cron from "node-cron";
+import { transferPastAppointments } from "./transferPastAppointments";
+
+export function cronJobs() {
+  cron.schedule(
+    "59 23 * * *",
+    async () => {
+      console.log("Running transfer past appointments");
+      await transferPastAppointments();
+      console.log("Finished running pastAppointments");
+    },
+    {
+      timezone: "America/Los_Angeles",
+    }
+  );
+}
+
 export * from "./transferPastAppointments";
