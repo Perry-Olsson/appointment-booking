59efe66b3ae98f3e9af32c35ac214160af5db037
Perry Olsson <olsson.perry@gmail.com>
2021-06-27 12:59:41 -0700

    added favicon

--- appointment-booking-client/src/components/HeadTags.tsx
+++ appointment-booking-client/src/components/HeadTags.tsx
@@ -8,6 +8,9 @@ export const HeadTags: React.FC<HeadTagsProps> = ({ children }) => {
         name="viewport"
         content="width=device-width, initial-scale=1.0, minimum-scale=1.0, user-scalable=no"
       />
+      <meta name="description" content="An online scheduling application MVP" />
+      <meta name="author" content="Perry Olsson" />
+      <link rel="icon" type="image/png" href="/favicon.png" />
       {children}
     </Head>
   );
