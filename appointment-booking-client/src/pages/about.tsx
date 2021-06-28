65fbeb00c0408c4b469eac3bf0ecd018f1f8cbfe
Perry Olsson <olsson.perry@gmail.com>
2021-06-28 11:46:41 -0700

    deployment script working

--- appointment-booking-client/src/pages/about.tsx
+++ appointment-booking-client/src/pages/about.tsx
@@ -1,5 +1,11 @@
 export default function about() {
-  return <h1 style={{ display: "flex", justifyContent: "center" }}>About</h1>;
+  return (
+    <h1
+      style={{ display: "flex", justifyContent: "center", marginTop: "50px" }}
+    >
+      About
+    </h1>
+  );
 }
 
 about.displayName = "about";
--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,8 @@ docker-compose build
 
 docker-compose push
 
-~/.ssh/deploy-appointment-booking.sh
+ssh -i ~/.ssh/perry-ubuntu-desktop.pem ubuntu@ec2-3-21-57-64.us-east-2.compute.amazonaws.com \
+"sudo docker stack deploy -c ./appointment-booking/docker-compose.yml --prune app \
+&& ./appointment-booking/prune.sh"
+
+exit;
