e88484af944feebc19e93445a63bba0d07a2a6a8
Perry Olsson <olsson.perry@gmail.com>
2021-07-01 09:42:14 -0700

    fixed svg animation bug

--- appointment-booking-client/src/components/SuccessMark.tsx
+++ appointment-booking-client/src/components/SuccessMark.tsx
@@ -7,80 +7,47 @@ interface Props {
 
 export const SuccessMark: FC<Props> = ({ size = 300 }) => {
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        height: "100%",
-        width: "100%",
-        alignItems: "center",
-      }}
-    >
+    <Container>
       <svg
         id="check"
         version="1.1"
-        xmlns="http://www.w3.org/2000/svg"
-        xmlnsXlink="http://www.w3.org/1999/xlink"
-        preserveAspectRatio="xMidYMid meet"
         viewBox="0 0 640 640"
         width={size}
         height={size}
       >
-        <defs>
-          <CirclePath
-            // ref={CircleRef}
-            d="M220 320C220 264.81 264.81 220 320 220C375.19 220 420 264.81 420 320C420 375.19 375.19 420 320 420C264.81 420 220 375.19 220 320Z"
-            id="bNDcVv7yE"
-          ></CirclePath>
-          <CheckPath
-            // ref={CheckRef}
-            d="M267 323.25C288.91 345.47 302.61 359.36 308.09 364.91C308.49 365.32 309.16 365.27 309.5 364.82C309.06 365.39 329.78 337.79 371.65 282"
-            id="d2YDwSkAM"
-          ></CheckPath>
-        </defs>
-        <g>
-          <g>
-            <g>
-              <g>
-                <use
-                  xlinkHref="#bNDcVv7yE"
-                  opacity="1"
-                  fillOpacity="0"
-                  stroke="#24ff00"
-                  strokeWidth="11"
-                  strokeOpacity="1"
-                ></use>
-              </g>
-            </g>
-            <g>
-              <g>
-                <use
-                  xlinkHref="#d2YDwSkAM"
-                  opacity="1"
-                  fillOpacity="0"
-                  stroke="#24ff00"
-                  strokeWidth="11"
-                  strokeOpacity="1"
-                ></use>
-              </g>
-            </g>
-          </g>
-        </g>
+        <CirclePath
+          d="M220 320C220 264.81 264.81 220 320 220C375.19 220 420 264.81 420 320C420 375.19 375.19 420 320 420C264.81 420 220 375.19 220 320Z"
+          opacity="1"
+          fillOpacity="0"
+          stroke="#24ff00"
+          strokeWidth="11"
+          strokeOpacity="1"
+        ></CirclePath>
+        <CheckPath
+          d="M267 323.25C288.91 345.47 302.61 359.36 308.09 364.91C308.49 365.32 309.16 365.27 309.5 364.82C309.06 365.39 329.78 337.79 371.65 282"
+          opacity="1"
+          fillOpacity="0"
+          stroke="#24ff00"
+          strokeWidth="11"
+          strokeOpacity="1"
+        ></CheckPath>
       </svg>
-    </div>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 100%;
+  width: 100%;
+  align-items: center;
+`;
 
 const CirclePath = styled.path`
   stroke-dasharray: 628;
   stroke-dashoffset: 628;
-  -webkit-animation: dash 0.8s linear forwards;
   animation: dash 0.8s linear forwards;
-  @-webkit-keyframes dash {
-    to {
-      stroke-dashoffset: 0;
-    }
-  }
   @keyframes dash {
     to {
       stroke-dashoffset: 0;
@@ -91,13 +58,7 @@ const CirclePath = styled.path`
 const CheckPath = styled.path`
   stroke-dasharray: 170;
   stroke-dashoffset: 170;
-  -webkit-animation: dash 0.8s linear 0.9s forwards;
   animation: dash 0.8s linear 0.9s forwards;
-  @-webkit-keyframes dash {
-    to {
-      stroke-dashoffset: 0;
-    }
-  }
   @keyframes dash {
     to {
       stroke-dashoffset: 0;
--- appointment-booking-client/src/pages/index.tsx
+++ appointment-booking-client/src/pages/index.tsx
@@ -1,14 +1,10 @@
-import { SuccessMark } from "../components";
 export default function home() {
   return (
-    <>
-      <h1
-        style={{ display: "flex", justifyContent: "center", marginTop: "50px" }}
-      >
-        Home
-      </h1>
-      <SuccessMark />
-    </>
+    <h1
+      style={{ display: "flex", justifyContent: "center", marginTop: "50px" }}
+    >
+      Home
+    </h1>
   );
 }
 
